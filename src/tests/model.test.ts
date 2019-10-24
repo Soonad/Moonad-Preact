905f7b9f05d37ba477b91c4c7669c8daf8c9ef0f
Bernardo Amorim <contato@bamorim.com>
2019-10-24 14:44:42 -0300

    test: add a simple test to start testing things

---
+++ src/tests/model.test.ts
@@ -0,0 +1,40 @@
+import { ModuleLoader, Token } from "../model";
+
+describe("ModuleLoader", () => {
+  let moduleLoader: ModuleLoader;
+
+  beforeEach(async () => {
+    moduleLoader = new ModuleLoader();
+  });
+
+  test("Can load local code with translated tokens", async () => {
+    const result = await moduleLoader.load_local(`a : Word 1 b : Word 2`);
+
+    const emptyToken: Token = { type: "txt", content: "" };
+    const spaceToken: Token = { type: "txt", content: " " };
+    const colonToken: Token = { type: "sym", content: ":" };
+    const wordTypeToken: Token = { type: "sym", content: "Word" };
+
+    const expectedTokens: Token[] = [
+      emptyToken,
+      { type: "def", content: "a", full_name: "local/a" },
+      spaceToken,
+      colonToken,
+      spaceToken,
+      wordTypeToken,
+      spaceToken,
+      { type: "num", content: "1" },
+      spaceToken,
+      { type: "def", content: "b", full_name: "local/b" },
+      spaceToken,
+      colonToken,
+      spaceToken,
+      wordTypeToken,
+      spaceToken,
+      { type: "num", content: "2" },
+      emptyToken
+    ];
+
+    expect(result.tokens).toEqual(expectedTokens);
+  });
+});
